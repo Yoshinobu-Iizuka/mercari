0aac8243cb40a0eb25b2e539feaf1286d97b8798
飯塚嘉宣 <rainetokomugi@gmail.com>
2019-01-20 19:56:43 +0900

    create_category_model
    ## WHAT
    Rails g model category
    アソシエーションの記述の追加
    
    ## WHY
    商品情報のカテゴリー情報を管理する為。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190120104607) do
+ActiveRecord::Schema.define(version: 20190120105415) do
 
   create_table "brands", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.text     "name",       limit: 65535
@@ -28,6 +28,12 @@ ActiveRecord::Schema.define(version: 20190120104607) do
     t.datetime "created_at",            null: false
     t.datetime "updated_at",            null: false
     t.index ["user_id"], name: "index_cards_on_user_id", using: :btree
+  end
+
+  create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
