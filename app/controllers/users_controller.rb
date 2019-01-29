79924d4d1152769cd71f40159bb6701013ddf312
m1011522 <m1011522@gmail.com>
2019-01-29 18:16:29 +0900

    add yml files using select-option-data

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,10 @@ class UsersController < ApplicationController
   end
 
   def show
+    require 'yaml'
+    @value = YAML.load_file('config/prefecture-select.yml')
+    @birthYear = YAML.load_file('config/birth-year.yml')
+    @birthMonth = YAML.load_file('config/birth-month.yml')
+    @birthDay = YAML.load_file('config/birth-day.yml')
   end
 end
