d9702608e8f724c4207224c2e5d07e2f63661d19
掛野広貴 <ryokucha1173@gmail.com>
2019-02-23 19:27:21 +0900

    Delete end

--- db/migrate/20190216164123_change_column_to_user.rb
+++ db/migrate/20190216164123_change_column_to_user.rb
@@ -1,20 +1,19 @@
 class ChangeColumnToUser < ActiveRecord::Migration[5.0]
   def change
-      change_column :users, :postal_code, :integer,       null: true
-      change_column :users, :prefecture, :string,         null: true
-      change_column :users, :city, :string,               null: true
-      change_column :users, :address_first, :string,      null: true
-      change_column :users, :address_second, :string,     null: true
-      change_column :users, :telephone, :string,          null: true
-    end
+    change_column :users, :postal_code, :integer,       null: true
+    change_column :users, :prefecture, :string,         null: true
+    change_column :users, :city, :string,               null: true
+    change_column :users, :address_first, :string,      null: true
+    change_column :users, :address_second, :string,     null: true
+    change_column :users, :telephone, :string,          null: true
+  end
 
-    def down
-      change_column :users, :postal_code, :integer,       null: false
-      change_column :users, :prefecture, :string,         null: false
-      change_column :users, :city, :string,               null: false
-      change_column :users, :address_first, :string,      null: false
-      change_column :users, :address_second, :string,     null: false
-      change_column :users ,:telephone, :string,          null: false
-    end
+  def down
+    change_column :users, :postal_code, :integer,       null: false
+    change_column :users, :prefecture, :string,         null: false
+    change_column :users, :city, :string,               null: false
+    change_column :users, :address_first, :string,      null: false
+    change_column :users, :address_second, :string,     null: false
+    change_column :users ,:telephone, :string,          null: false
   end
 end
