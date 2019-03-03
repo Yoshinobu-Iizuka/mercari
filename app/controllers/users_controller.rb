b6b8d2dcb8fe2c8b408694ac966b9130d2aad2ec
m1011522 <m1011522@gmail.com>
2019-03-03 22:50:31 +0900

    add prefecture select yaml

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,15 @@
 class UsersController < ApplicationController
   def index
   end
+
+  def logout
+  end
+
+  def new
+  end
+
+  def show
+    require 'yaml'
+    @value = YAML.load_file('config/prefecture-select.yml')
+  end
 end
