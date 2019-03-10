155cc293752853a57227e9fca4546fd2d05c775a
m1011522 <m1011522@gmail.com>
2019-03-10 21:11:04 +0900

    レスポンシブ対応

---
+++ app/assets/javascripts/sp_nav.js
@@ -0,0 +1,28 @@
+$(function () {
+
+  const $hamburger = document.querySelector('.hamburger');
+  $hamburger.addEventListener('click', function(){
+    $hamburger.classList.toggle('is-active')
+  });
+
+  const $wrapper = document.getElementById('nav-wrapper');
+  const $navBtn = document.getElementById('nav-btn');
+
+  $navBtn.addEventListener('click', navToggle);
+
+  function navToggle() {
+    if ($wrapper.classList.contains('nav-open')) {
+      navCloseFunc();
+    } else {
+      navOpenFunc();
+    }
+  }
+
+  function navOpenFunc() {
+    $wrapper.classList.add('nav-open');
+  }
+  function navCloseFunc() {
+    $wrapper.classList.remove('nav-open');
+  }
+
+})();
