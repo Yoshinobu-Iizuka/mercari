1e6ece5b43a3480e39688658ece66792a397f9f5
掛野広貴 <ryokucha1173@gmail.com>
2019-03-14 23:47:48 +0900

    Items_controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,6 +4,11 @@ class ItemsController < ApplicationController
   end
 
   def show
+    @item = Item.find(params[:id])
+    @items = Item.includes(:user).order("id DESC")
+    @image = Image.find(params[:id])
+    @images = @item.images.includes(:item)
+    @list = Image.includes(:item)
   end
 
   def confirm
