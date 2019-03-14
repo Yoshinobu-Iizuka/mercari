092c41dc02c535c1930e7e5b104d3c304f485dd7
掛野広貴 <ryokucha1173@gmail.com>
2019-03-14 23:48:07 +0900

    Edit item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,7 @@ class Item < ApplicationRecord
   belongs_to :category
   belongs_to :child_category
   belongs_to :grandchild_category
+  belongs_to :user
   has_one :trade
   has_one :card
   has_many :likes
@@ -10,4 +11,12 @@ class Item < ApplicationRecord
   has_many :categories, through: :item_categories
   has_many :item_categories
   has_many :images
+
+  def previous
+    Item.order('id DESC').where('id < ?',self.id).first
+  end
+
+  def next
+    Item.order('id ASC').where('? < id', self.id).first
+  end
 end
